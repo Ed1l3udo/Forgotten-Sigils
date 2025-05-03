3d6fa6835fabc3084ddacb0fb48e75b2fb290092
Ed1l3udo <edileudommfilho@gmail.com>
2025-05-03 08:51:28 -0300

    AudioMixer and MasterVolumeSlider sound

--- Assets/Menu/Scripts/AudioController.cs
+++ Assets/Menu/Scripts/AudioController.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class AudioController : MonoBehaviour
 {
+    [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private AudioSource audioSourceMainTheme;
     [SerializeField] private AudioSource selectionSoundUiAudioSource;
     [SerializeField] private AudioClip selectionSoundClip; 
     [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Slider masterVolumeSlider;
     void Start()
     {
         Load();
@@ -22,24 +25,38 @@ public class AudioController : MonoBehaviour
         }
     }
 
+    public void SetMasterVolume(float volume)
+    {
+        float volumeInDb = Mathf.Log10(Mathf.Clamp(volume / 10f, 0.0001f, 1f)) * 20f;
+        audioMixer.SetFloat("MasterVolume", volumeInDb);
+        PlayerPrefs.SetFloat("masterVolume", volume);
+        PlayerPrefs.Save();
+    }
+
     public void SetMusicVolume(float volume)
     {
-        float normalizedVolume = volume / 10f; // converte 1–10 para 0.1–1.0
-        audioSourceMainTheme.volume = normalizedVolume;
-        Save(volume);
+        float volumeInDb = Mathf.Log10(Mathf.Clamp(volume / 10f, 0.0001f, 1f)) * 20f;
+        audioMixer.SetFloat("MusicVolume", volumeInDb);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+        PlayerPrefs.Save();
     }
 
     private void Load()
     {
-        float savedMusicVolume = PlayerPrefs.GetFloat("musicVolume", 10f);
-        float normalizedVolume = savedMusicVolume / 10;
-        audioSourceMainTheme.volume = normalizedVolume;
-        musicVolumeSlider.value = savedMusicVolume; 
-    }
+        //Master Volume
+        float savedMasterVolume = PlayerPrefs.GetFloat("masterVolume", 10f);
+        float masterDb = Mathf.Log10(Mathf.Clamp(savedMasterVolume / 10f, 0.0001f, 1f)) * 20f;
+        audioMixer.SetFloat("MasterVolume", masterDb);
+        masterVolumeSlider.value = savedMasterVolume;
 
-    private void Save(float volume)
-    {
-        PlayerPrefs.SetFloat("musicVolume", volume);
-        PlayerPrefs.Save();
+
+
+        //Music Volume
+        float savedMusicVolume = PlayerPrefs.GetFloat("musicVolume", 10f);
+        float volumeInDb = Mathf.Log10(Mathf.Clamp(savedMusicVolume / 10f, 0.0001f, 1f)) * 20f;
+        audioMixer.SetFloat("MusicVolume", volumeInDb);
+        musicVolumeSlider.value = savedMusicVolume;
+
+
     }
 }

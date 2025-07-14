b516054f234e66024498d44231ce5c2d6541c7de
gabrielm__almeida <gabriel.matias.lg@gmail.com>
2025-07-14 11:51:28 -0300

    Updated the health UI

--- Assets/Game/Scripts/UI/HealthUI.cs
+++ Assets/Game/Scripts/UI/HealthUI.cs
@@ -4,30 +4,11 @@ using UnityEngine.UI;
 public class HealthUI : MonoBehaviour
 {
 
-    [SerializeField] private Image[] hearts;
-    [SerializeField] private Sprite fullHeart;
-    [SerializeField] private Sprite halfHeart;
-    [SerializeField] private Sprite hollowHeart;
+    [SerializeField] private Sprite[] hearts;
+    [SerializeField] private Image heartImage;
 
     public void UpdateUI(int currentHealth)
     {
-        for(int i = 1; i < hearts.Length * 2; i+=2)
-        {
-            if(i < currentHealth)
-            {
-                hearts[i/2].sprite = fullHeart;
-            }
-            else
-            {
-                if(i-1 < currentHealth)
-                {
-                    hearts[i/2].sprite = halfHeart;
-                }
-                else
-                {
-                    hearts[i/2].sprite = hollowHeart;
-                }
-            }
-        }
+        heartImage.sprite = hearts[currentHealth - 1];
     }
 }

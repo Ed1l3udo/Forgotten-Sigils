53fb42f9737ef9ca7dd157b1353fe72ab04b9636
gabrielm__almeida <gabriel.matias.lg@gmail.com>
2025-07-16 11:33:00 -0300

    Startet on new Cave scene and started on GameManager object

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    // Dados do jogador
+    public int playerMaxHealth = 5;
+    public int playerCurrentHealth;
+    public int playerMaxMana = 27;
+    public int playerCurrentMana;
+
+    void Awake()
+    {
+        // Implementação do Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            playerCurrentHealth = playerMaxHealth; // Inicializa
+            playerCurrentMana = playerMaxMana;
+        }
+        else
+        {
+            Destroy(gameObject); // Evita duplicação
+        }
+    }
+}
